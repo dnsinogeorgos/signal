3935b77baa430e6d1001406989715eeb2d21403e
Dimitrios-Nikolaos Sinogeorgos <dnsinogeorgos@protonmail.ch>
2021-07-11 13:29:27 +0300

    Flag inprogress (#1)
    * ignore signal if handler is in progress
    
    * part 2
    
    * part 3 - used a semaphore

--- main.go
+++ main.go
@@ -14,12 +14,13 @@ type Signal struct {
 	Exit    bool
 	Code    int
 	Handler func()
+	sem     chan struct{}
 }
 
 // Handle sets up the wiring and starts a goroutine to listen in the background. It
 // receives a slice of signals to handle. Signals are iterated in the order provided and
 // only the first signal that matches is handled.
-func Handle(ss []Signal) {
+func Handle(ss []*Signal) {
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh)
 
@@ -27,13 +28,23 @@ func Handle(ss []Signal) {
 }
 
 // listen iterates over the list of provided signals to act upon on each invocation.
-func listen(c chan os.Signal, ss []Signal) {
+func listen(c chan os.Signal, ss []*Signal) {
+	for _, s := range ss {
+		s.sem = make(chan struct{}, 1)
+	}
+
 	for {
 		rs := <-c
 
-		for _, sig := range ss {
-			if rs == sig.Signal {
-				go handleSignal(sig)
+		for _, s := range ss {
+			if rs == s.Signal {
+				if len(s.sem) != 0 {
+					log.Printf("ignoring %s, a handler is in progress", s.Signal.String())
+					break
+				}
+
+				s.sem <- struct{}{}
+				go handleSignal(s)
 				break
 			}
 		}
@@ -41,7 +52,7 @@ func listen(c chan os.Signal, ss []Signal) {
 }
 
 // handleSignal executes the behaviour described in the Signal value.
-func handleSignal(s Signal) {
+func handleSignal(s *Signal) {
 	log.Printf(s.Msg)
 	if s.Handler != nil {
 		s.Handler()
@@ -49,4 +60,5 @@ func handleSignal(s Signal) {
 	if s.Exit {
 		os.Exit(s.Code)
 	}
+	<-s.sem
 }
